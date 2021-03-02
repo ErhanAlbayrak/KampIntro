d2ec61dd8447aa01754bf4c2c4a6243545f07ce8
Erhan Albayrak <eranalbayrak@gmail.com>
2021-03-02 14:38:58 +0300

    Ders Sonu

---
+++ Matematik/DortIslem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matematik
+{
+    class DortIslem
+    {
+        public void topla(int a,int b) 
+        {
+            int toplam = a + b;
+            Console.WriteLine(toplam);
+        }
+    }
+}
